31694bd8a31b789699e131bdeb477b801056d701
AussieGuy0 <anthony.bruno196@gmail.com>
2019-01-12 16:29:53 +1030

    Move scripts to _scripts dir, update Gemfile

--- _scripts/createPost.sh
+++ _scripts/createPost.sh
@@ -2,17 +2,22 @@
 # Creates a new post with todays date, and set the title to the first argument 
 # given when running the script. Also makes a directory in _includes for code 
 # samples
+set -e
 
 DATE=$(date "+%Y-%m-%d")
-
 TITLE=$1
+CLEANED_TITLE=${TITLE// /-}
+FILE=$DATE"-$CLEANED_TITLE.md"
+POSTPATH="../_posts/$FILE"
+CURR_DIR=$(dirname "$0")
 
-FILE=$DATE"-$1.md"
-
-POSTPATH="../_posts/${FILE// /-}"
-
+if [[ -z $TITLE ]]
+then
+    echo "Usage: ./makePost.sh <title>"
+    exit 1
+fi
+cd "$CURR_DIR"
 cp post.txt "$POSTPATH"
-
 sed -i -e "s/{title}/$TITLE/g" "$POSTPATH"
-
-mkdir "../_includes/$TITLE"
+mkdir "../_includes/$CLEANED_TITLE"
+vim "$POSTPATH"
